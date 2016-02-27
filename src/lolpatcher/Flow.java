d6fb32c0955b21612f649d7b83886337c756fcba
Rick Hendricksen <Rick.Hendricksen@gmail.com>
2016-02-27 11:03:24 +0100

    Fix flow animation at window borders

--- src/lolpatcher/Flow.java
+++ src/lolpatcher/Flow.java
@@ -7,13 +7,18 @@ import nl.xupwup.Util.ShaderProgram;
 import nl.xupwup.Util.Texture;
 import org.lwjgl.opengl.GL11;
 import static org.lwjgl.opengl.GL11.GL_BLEND;
+import static org.lwjgl.opengl.GL11.GL_NEAREST;
 import static org.lwjgl.opengl.GL11.GL_QUADS;
 import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_MAG_FILTER;
 import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
 import static org.lwjgl.opengl.GL11.glBegin;
 import static org.lwjgl.opengl.GL11.glEnd;
 import static org.lwjgl.opengl.GL11.glTexCoord2f;
+import static org.lwjgl.opengl.GL11.glTexParameteri;
 import static org.lwjgl.opengl.GL11.glVertex2f;
+import org.lwjgl.opengl.GL12;
+import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
 import static org.lwjgl.opengl.GL30.GL_RG;
@@ -39,6 +44,15 @@ public class Flow {
         this.screenBuffer = screenBuffer;
         framebuffer1 = new FrameBuffer(width, height, GL_TEXTURE_RECTANGLE, GL30.GL_RG16F, GL_RG);
         framebuffer2 = new FrameBuffer(width, height, GL_TEXTURE_RECTANGLE, GL30.GL_RG16F, GL_RG);
+        GL11.glBindTexture(GL_TEXTURE_RECTANGLE, framebuffer1.framebuffertex);
+        glTexParameteri(GL_TEXTURE_RECTANGLE, GL11.GL_TEXTURE_WRAP_S, GL13.GL_CLAMP_TO_BORDER);
+        glTexParameteri(GL_TEXTURE_RECTANGLE, GL11.GL_TEXTURE_WRAP_T, GL13.GL_CLAMP_TO_BORDER);
+        // border color defaults to 0,0,0,0, which is fine
+        GL11.glBindTexture(GL_TEXTURE_RECTANGLE, framebuffer2.framebuffertex);
+        glTexParameteri(GL_TEXTURE_RECTANGLE, GL11.GL_TEXTURE_WRAP_S, GL13.GL_CLAMP_TO_BORDER);
+        glTexParameteri(GL_TEXTURE_RECTANGLE, GL11.GL_TEXTURE_WRAP_T, GL13.GL_CLAMP_TO_BORDER);
+        // border color defaults to 0,0,0,0, which is fine
+        GL11.glBindTexture(GL_TEXTURE_RECTANGLE, 0);
         sp = ShaderProgram.getFromStream(this.getClass().getResourceAsStream("resources/flow.frag"),
                 this.getClass().getResourceAsStream("resources/flow.vert"));
         sp2 = ShaderProgram.getFromStream(this.getClass().getResourceAsStream("resources/texture.frag"),
